a3bb298de110f960c9f042e9f2260c0d9b40efa8
Matthew James Briggs <matthew.james.briggs@gmail.com>
2019-04-02 11:59:34 -0700

    log closer

--- log_err.go
+++ log_err.go
@@ -3,36 +3,20 @@
 package flog
 
 import (
+	"io"
+
 	"github.com/sirupsen/logrus"
 )
 
-// LogErr can wrap a function that returns an error in cases where you do not want to handle ther error.
+// LogClose can wrap a function that returns an error in cases where you do not want to handle ther error.
 // for example 'defer LogErr(file.Close())'
-func LogErr(err error) {
+func LogClose(closer io.Closer) {
+	err := closer.Close()
+
 	if err == nil {
 		return
 	}
 
 	caller := Caller(2)
-	logrus.Errorf("%s - unhandled error: %s", caller, err.Error())
-}
-
-// LogErr2 can wrap a function that returns an error and something else we don't care about
-func LogErr2(ignored interface{}, err error) {
-	if err == nil {
-		return
-	}
-
-	caller := Caller(2)
-	logrus.Errorf("%s - unhandled error: %s", caller, err.Error())
-}
-
-// LogErr2 can wrap a function that returns an error and something else we don't care about
-func LogErr3(ignored1, ignored2 interface{}, err error) {
-	if err == nil {
-		return
-	}
-
-	caller := Caller(2)
-	logrus.Errorf("%s - unhandled error: %s", caller, err.Error())
+	logrus.Errorf("%s - unhandled io.Closer error: %s", caller, err.Error())
 }

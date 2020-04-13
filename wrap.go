fbaf8c7362d53b6c7e5dcbb5e3eba0c1e0d7fb5e
Matt Briggs <matthew.james.briggs@gmail.com>
2020-04-12 17:47:23 -0700

    allow wrap to take a nil err

--- wrap.go
+++ wrap.go
@@ -3,6 +3,9 @@ package flog
 import "errors"
 
 func Wrap(err error) error {
+	if err == nil {
+		return nil
+	}
 	caller := Caller(2)
 	str := caller + " - error - " + err.Error()
 	return errors.New(str)
